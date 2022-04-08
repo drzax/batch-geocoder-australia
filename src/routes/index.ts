a7d53f483637296d80850eea8b2f8e51684d2fae
Simon Elvery <simon@elvery.net>
2022-04-08 15:16:10 +1000

    Try to detect error

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,28 @@
 import { geocode, type GeocodeResult } from '$lib/geocoder';
 
 export const post = async ({ request }) => {
-	const addressField = (await request.formData()).get('addresses') as string;
-	const addresses = addressField.split('\n').map((address: string) => address.trim());
+	try {
+		const addressField = (await request.formData()).get('addresses') as string;
+		const addresses = addressField.split('\n').map((address: string) => address.trim());
 
-	const results = await addresses.reduce<Promise<GeocodeResult[]>>(async (acc, address, i) => {
-		const memo = await acc;
-		const result =
-			address.length > 3
-				? await geocode(address, 1).catch((err) => {
-						console.error(err);
-						return null;
-				  })
-				: null;
-		memo.push(result);
-		return memo;
-	}, Promise.resolve([]));
+		const results = await addresses.reduce<Promise<GeocodeResult[]>>(async (acc, address, i) => {
+			const memo = await acc;
+			const result =
+				address.length > 3
+					? await geocode(address, 1).catch((err) => {
+							console.error(err);
+							return null;
+					  })
+					: null;
+			memo.push(result);
+			return memo;
+		}, Promise.resolve([]));
 
-	const result = results.map((r) => (r ? JSON.stringify(r.results[0]) : null)).join('\n');
+		const result = results.map((r) => (r ? JSON.stringify(r.results[0]) : null)).join('\n');
 
-	return { code: 200, body: { results: result, addresses: addressField } };
+		return { code: 200, body: { results: result, addresses: addressField } };
+	} catch (e) {
+		console.error(e);
+		return { code: 500, body: { error: e.message } };
+	}
 };
